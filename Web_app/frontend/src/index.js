947ee273454bc72ebf22f880e1718dd96abf18a3
Avanish Dubey <61058396+avanishd4@users.noreply.github.com>
2022-11-24 12:02:18 +0530

    Update index.js

--- Web_app/frontend/src/index.js
+++ Web_app/frontend/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
+import '@fortawesome/fontawesome-free/css/all.min.css'; 
+import 'bootstrap-css-only/css/bootstrap.min.css';
+import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
