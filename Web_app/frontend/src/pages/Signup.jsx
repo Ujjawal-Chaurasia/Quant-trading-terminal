fbd85eebbe7532eb6f34011cb6aa73125b717bbf
Avanish Dubey <61058396+avanishd4@users.noreply.github.com>
2022-11-24 12:01:47 +0530

    Update Signup.jsx

--- Web_app/frontend/src/pages/Signup.jsx
+++ Web_app/frontend/src/pages/Signup.jsx
@@ -1,39 +1,77 @@
-import React from 'react'
+import React from 'react';
+import {
+  MDBBtn,
+  MDBContainer,
+  MDBRow,
+  MDBCol,
+  MDBCard,
+  MDBCardBody,
+  MDBCardImage,
+  MDBInput,
+  MDBIcon,
+  MDBCheckbox
+}
+from 'mdb-react-ui-kit';
 
-const Signup = () => {
+function App() {
   return (
-    <div id='signuppage'>
-        <div id='leftpane'>
+    <MDBContainer fluid>
 
-        </div>
-        <div id='formarea'>
-            <div>
-                <form>
-  <label>
-    Name:
-    <input type="text" name="name" />
-  </label>
-  <form>
-  <label>
-    E-mail:
-    <input type="text" name="name" />
-  </label>
+      <MDBCard className='text-black m-5' style={{borderRadius: '50px'}}>
+        <MDBCardBody>
+          <MDBRow>
+            <MDBCol md='10' lg='6' className='order-2 order-lg-1 d-flex flex-column align-items-center'>
 
-</form>
-<form>
-  <label>
-    Password:
-    <input type="text" name="name" />
-  </label>
-  <input type="submit" value="Submit" />
-</form>
+            <div className="text-center">
+              <img src="https://thumbs.dreamstime.com/z/business-trading-logo-vector-design-171734765.jpg"
+                style={{width: '250px'}} alt="logo" />
+              <h4 className="mt-1 mb-5 pb-1">QUANT TRADING TERMINAL</h4>
+              <p classNAme="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Sign up</p>
 
- 
-</form>
-            </div>
-        </div>
-    </div>
-  )
+            
+              </div>
+
+           
+
+
+              <div className="d-flex flex-row align-items-center mb-4 ">
+                <MDBIcon fas icon="user me-3" size='lg'/>
+                <MDBInput label='Your Name' id='form1' type='text' className='w-100'/>
+              </div>
+
+              <div className="d-flex flex-row align-items-center mb-4">
+                <MDBIcon fas icon="envelope me-3" size='lg'/>
+                <MDBInput label='Your Email' id='form2' type='email'/>
+              </div>
+
+              <div className="d-flex flex-row align-items-center mb-4">
+                <MDBIcon fas icon="lock me-3" size='lg'/>
+                <MDBInput label='Password' id='form3' type='password'/>
+              </div>
+
+              <div className="d-flex flex-row align-items-center mb-4">
+                <MDBIcon fas icon="key me-3" size='lg'/>
+                <MDBInput label='Repeat your password' id='form4' type='password'/>
+              </div>
+
+              <div className='mb-4'>
+                <MDBCheckbox name='flexCheck' value='' id='flexCheckDefault' label='Subscribe to our newsletter' />
+              </div>
+
+              <MDBBtn className='mb-4' size='lg'>Register</MDBBtn>
+
+            </MDBCol>
+
+            <MDBCol md='10' lg='6' className='order-1 order-lg-2 d-flex align-items-center'>
+              <MDBCardImage src='https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-registration/draw1.webp' fluid/>
+            </MDBCol>
+
+          </MDBRow>
+        </MDBCardBody>
+      </MDBCard>
+
+    </MDBContainer>
+  );
 }
 
-export default Signup
+export default App;
