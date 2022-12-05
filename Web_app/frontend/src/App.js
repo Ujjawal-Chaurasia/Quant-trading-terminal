add94c9f673d71571669610385d251df2772274a
s166harth <siddharthsagar2019@gmail.com>
2022-12-06 00:01:43 +0530

    second strategy

--- Web_app/frontend/src/App.js
+++ Web_app/frontend/src/App.js
@@ -5,8 +5,8 @@ import Signup from './pages/Signup';
 function App() {
   return (
     <div className="App">
-     {/* <Welcomepage/>    */}
-     <Signup/>
+     <Welcomepage/>   
+     {/* <Signup/> */}
      </div>
   );
 }
